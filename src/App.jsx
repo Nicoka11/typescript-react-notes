057b887fa0d5cb6e67ee34afe0af5321880e5706
Nicoka11 <nicoka11@yahoo.fr>
2021-07-24 21:43:11 +0200

    Note added to main App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import AddNote from "./components/AddNote";
 import styled from "styled-components";
 import colorPalette from "./assets/colorPalette.js";
+
+import AddNote from "./components/AddNote";
+import Note from "./components/Note";
 
 const AppWrapper = styled.div`
   padding-top: 3rem;
@@ -19,6 +21,26 @@ function ReactApp() {
     <AppWrapper>
       <App>
         <AddNote btn="Add a note" />
+        <Note
+          title="This is the title"
+          content="
+        Studio Photo
+        Bureau
+        House
+        Montagnes
+        Arret de Metro ☑️
+        Salle de bain
+        City 
+        Park 
+        Cyberpunk Bar
+        American 80s Bar (Insidoe/Outside) ☑️
+        Cyberpunk Room (w/ a floating bed)
+        Village
+        Factory
+        Ville/Maison/Temple du Japon
+        Medieval"
+          color={colorPalette.BananaMania}
+        />
       </App>
     </AppWrapper>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,21 @@
-const Note = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import styled from "styled-components";
 
-export default Note
+const NoteCard = styled.div.attrs((props) => ({
+  color: props.color,
+}))`
+  background-color: ${(props) => props.color};
+  width: 15rem;
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+`;
+
+const Note = (props) => {
+  return (
+    <NoteCard color={props.color}>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </NoteCard>
+  );
+};
+
+export default Note;
